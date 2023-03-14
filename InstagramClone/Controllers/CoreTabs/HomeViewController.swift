b26d74b7b8b9fde2934dc75c0b47b77f94fdcfd8
subashsethuramana <sethuraman.achi@gmail.com>
2023-03-14 06:22:06 +0530

    Initial Commit

---
+++ InstagramClone/Controllers/CoreTabs/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Subash Sethuraman A on 14/03/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
---
+++ InstagramClone/Controllers/CoreTabs/NotificationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationViewController.swift
+//  InstagramClone
+//
+//  Created by Subash Sethuraman A on 14/03/23.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
